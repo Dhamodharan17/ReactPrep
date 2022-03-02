107226b1dfce9a304225efc738700a79ae28ba21
Karthik <30789057+Dhamodharan17@users.noreply.github.com>
2022-03-02 22:27:26 +0530

    Update ReactPost1.js

--- ReactPost1.js
+++ ReactPost1.js
@@ -40,22 +40,81 @@ const App = <div>{validateAge(18)}</div>;
 
 ReactDOM.render(App, document.getElementById("root"));
 
+conditional rendering
+---------------------
+  
+  1. using ternary operator
+  
+import React, { Component } from 'react'
+export default class App extends Component {
 
-ES6
----
-1.Let vs const
-function printPrice(){
-    {
-        let price = 100;
-    }
-    console.log(price)
+  state = {
+    isDisplay: false
+  }
+
+  handleChange = () =>{
+    this.setState({isDisplay:!this.state.isDisplay})
+  }
+  render() {
+    return (
+      <div>
+       {this.state.isDisplay?<p>Condtion1</p>:<p>Condtion2</p>}
+       <button onClick={this.handleChange}>Click to Change</button>
+      </div>
+    )
+  }
 }
 
-printPrice() // Output : ReferenceError: price is not defined
+2. using logical and operator
 
-function printPrice(){
-    {
-        var price = 100;
-    }
-    console.log(price) //Output : 100
+import React, { Component } from 'react'
+export default class App extends Component {
+
+  state = {
+    isDisplay: false
+  }
+
+  handleChange = () =>{
+    this.setState({isDisplay:!this.state.isDisplay})
+  }
+  render() {
+    return (
+      <div>
+      {this.state.isDisplay&&<p>Displayed</p>}
+       <button onClick={this.handleChange}>Click to Change</button>
+      </div>
+    )
+  }
 }
+
+3.using if else
+  import React, { Component } from 'react'
+export default class App extends Component {
+
+  state = {
+    count: 0
+  }
+
+  handleChange = () =>{
+    this.setState({count:this.state.count+1})
+  }
+
+  render() {
+    let conditional_element = null;
+    if(this.state.count % 5 == 0){
+      conditional_element = <p>You got offer</p>
+    }else{
+      conditional_element = <p>wait got offer</p>
+    }
+    return (
+      <div>
+        Count : {this.state.count}
+      {conditional_element}
+       <button onClick={this.handleChange}>Click to Change</button>
+      </div>
+    )
+  }
+}
+
+
+  
